3288a790b7e7d4ac5d521622b2f32d9f59363890
Pelle <pelle.park@gmail.com>
2020-05-11 09:38:08 +0200

    Initial commit

---
+++ src/test/java/nordic_motorhome_project/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nordic_motorhome_project.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
