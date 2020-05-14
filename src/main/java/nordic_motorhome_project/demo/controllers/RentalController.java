a0251fe710404eb690cfad800ab8d4fc985b3793
Frederic <fred8442@stud.kea.dk>
2020-05-14 15:20:12 +0200

    Getmappings for controllers lavet

--- src/main/java/nordic_motorhome_project/demo/controllers/RentalController.java
+++ src/main/java/nordic_motorhome_project/demo/controllers/RentalController.java
@@ -1,7 +1,9 @@
 package nordic_motorhome_project.demo.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class RentalController {
 
     @GetMapping("/rental")
