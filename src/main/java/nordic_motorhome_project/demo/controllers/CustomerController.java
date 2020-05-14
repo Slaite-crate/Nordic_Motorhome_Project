9ef3455ac48cd38748c22d8c79e46d8a2a4c4a4c
ceci138p <ceci138p@stud.kea.dk>
2020-05-14 22:22:37 +0200

    Get mapping for read one customer

--- src/main/java/nordic_motorhome_project/demo/controllers/CustomerController.java
+++ src/main/java/nordic_motorhome_project/demo/controllers/CustomerController.java
@@ -5,6 +5,7 @@ import nordic_motorhome_project.demo.repositories.ICustomerRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CustomerController {
@@ -23,7 +24,12 @@ public class CustomerController {
     @GetMapping("/customer/list")
     public String customersList(Model model){
         model.addAttribute("customers",customerRepository.readAll());
-        return "../customer/list";
+        return "customer/list";
+    }
+    @GetMapping("/customer/read")
+    public String readCustomer(@RequestParam int id, Model model){
+        model.addAttribute("customers",customerRepository.read(id));
+        return "customer/read";
     }
 
 }
