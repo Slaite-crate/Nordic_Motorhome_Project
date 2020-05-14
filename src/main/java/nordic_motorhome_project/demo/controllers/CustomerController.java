4ca4988515f99c5c06e7eab92aed58ab6c39367b
ceci138p <ceci138p@stud.kea.dk>
2020-05-14 16:45:49 +0200

    Get mapping for read all customer

--- src/main/java/nordic_motorhome_project/demo/controllers/CustomerController.java
+++ src/main/java/nordic_motorhome_project/demo/controllers/CustomerController.java
@@ -1,13 +1,43 @@
 package nordic_motorhome_project.demo.controllers;
 
+import nordic_motorhome_project.demo.repositories.CustomerRepositoryImpl;
+import nordic_motorhome_project.demo.repositories.ICustomerRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CustomerController {
 
+    private ICustomerRepository customerRepository;
+
+    public CustomerController(){
+        customerRepository = new CustomerRepositoryImpl();
+    }
+
     @GetMapping("/customer")
     public String customer(){
         return "/customer/customer";
     }
+
+    @GetMapping("/customer/list")
+    public String customersList(Model model){
+        model.addAttribute("customers",customerRepository.readAll());
+        return "../customer/list";
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
