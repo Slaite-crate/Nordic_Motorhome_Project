f499802fc065aeff25e1b915b74d4d8063bb97ef
ceci138p <ceci138p@stud.kea.dk>
2020-05-26 11:25:58 +0200

    ModifierController

--- src/main/java/nordic_motorhome_project/demo/controllers/ModifierController.java
+++ src/main/java/nordic_motorhome_project/demo/controllers/ModifierController.java
@@ -1,7 +1,21 @@
 package nordic_motorhome_project.demo.controllers;
 
+import nordic_motorhome_project.demo.interfaceRepositories.IModifierRepository;
+import nordic_motorhome_project.demo.repositories.ModifierRespository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ModifierController {
+    private IModifierRepository modifierRepository;
+
+    public ModifierController(){
+        modifierRepository = new ModifierRespository();
+    }
+    @GetMapping("/modifier")
+    public String modifierList(Model model){
+        model.addAttribute("modifier",modifierRepository.readAll());
+        return "/modifier/modifier";
+    }
 }
